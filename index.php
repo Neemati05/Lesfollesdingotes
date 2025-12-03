158b9cdbd5734addefa13200323b11d1aca3b467
arwen91 <arwencap@yahoo.com>
2025-12-03 17:09:57 +0100

    page d'accueil

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Deuxième page</title>
+</head>
+<body>
+
+    <h1>Page PHP du projet</h1>
+    <p>Voici la deuxième page créée en PHP.</p>
+
+    <a href="index.html">Retour à la page d'accueil</a>
+
+</body>
+</html>
